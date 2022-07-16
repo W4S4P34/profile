bbd09f145d2a6dfef18cf18e7eededbef06d282c
La Thành Thái <lathanhthai1905@gmail.com>
2022-07-16 14:54:21 +0700

    Update NoMatch route

---
+++ src/pages/NoMatch/index.js
@@ -0,0 +1,9 @@
+const NoMatch = () => {
+  return (
+    <div className="d-flex mx-auto my-auto align-self-center">
+      Whoops, mate! You`&apos;`ve lost, may I lead you back?
+    </div>
+  );
+};
+
+export default NoMatch;
---
+++ src/pages/NoMatch/route.js
@@ -0,0 +1,9 @@
+import { lazy } from "react";
+
+export default {
+  path: "*",
+  index: false,
+  caseSensitive: false,
+  element: lazy(() => import(".")),
+  props: null
+};
